b176b77479bafa2948796248b65f3d232a37bb71
EdwardCoventry <60180868+EdwardCoventry@users.noreply.github.com>
2022-11-18 13:54:21 +0000

    config paths relative to __file__

--- katakana/getconfig.py
+++ katakana/getconfig.py
@@ -2,7 +2,9 @@ import os.path
 import yaml
 
 
-def get_config(path='katakana', name='config.yaml'):
+default_path = os.path.join(__file__, '..')
+
+def get_config(path=default_path, name='config.yaml'):
 	# Read YAML file
 	with open(os.path.join(path, name), 'r') as stream:
 		return yaml.safe_load(stream)
@@ -12,13 +14,13 @@ def write_config(config, path, name='config.yaml'):
 	with open(os.path.join(path, name), 'w+', encoding='utf8') as outfile:
 		yaml.dump(config, outfile, default_flow_style=False, allow_unicode=True)
 
-def get_training_config(path='katakana'):
+def get_training_config(path=default_path):
 	return get_config(path, 'trainingconfig.yaml')
 
-def get_use_model_config(path='katakana'):
+def get_use_model_config(path=default_path):
 	return get_config(path, 'usemodelconfig.yaml')
 
-def get_model_config(path='katakana'):
+def get_model_config(path=default_path):
 	return get_config(path, 'modelconfig.yaml')
 
 def write_model_config(config, path, name='modelconfig.yaml'):
