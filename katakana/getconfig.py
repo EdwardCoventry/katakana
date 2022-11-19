c144d98fc5bf33e6f7149410e755f125a19a9669
EdwardCoventry <60180868+EdwardCoventry@users.noreply.github.com>
2022-11-19 08:33:08 +0000

    made config not use root path

--- katakana/getconfig.py
+++ katakana/getconfig.py
@@ -11,7 +11,7 @@ def get_config(path='.', name='config.yaml'):
 
 def write_config(config, path, name='config.yaml'):
 	"""  Write YAML file  """
-	with open(os.path.join(root_path, path, name), 'w+', encoding='utf8') as outfile:
+	with open(os.path.join(path, name), 'w+', encoding='utf8') as outfile:
 		yaml.dump(config, outfile, default_flow_style=False, allow_unicode=True)
 
 def get_training_config(path='.'):
