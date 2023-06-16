9394de6bcdd8811d1d6f317ea9b604c49bb50222
EdwardCoventry <60180868+EdwardCoventry@users.noreply.github.com>
2023-06-16 17:22:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     license='MIT',
     url='http://github.com/wanasit/katakana',
     author='wanasit',
-    packages=['katakana'],
+    package_data={'katakana': ['usemodelconfig.yaml']},
+    package_data={'': ['usemodelconfig.yaml']},
     install_requires=['keras', 'h5py', 'numpy'],
 )
