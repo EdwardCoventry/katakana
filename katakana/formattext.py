e6fcfc28ee910115c41da04b28eb8567c4f96dc4
EdwardCoventry <60180868+EdwardCoventry@users.noreply.github.com>
2024-07-10 19:33:59 +0100

    added r strings

--- katakana/formattext.py
+++ katakana/formattext.py
@@ -5,12 +5,12 @@ import unidecode
 def format_text(text, convert_to_lower, convert_to_unidecode):
 
     if convert_to_unidecode:
-        if re.search('([一-龯])', text):
+        if re.search(r'([一-龯])', text):
             """ kanji """
             raise Exception('kanji in katakana')
-        if re.search('[\u3040-\u309F\u30A0-\u30FF]', text):
+        if re.search(r'[\u3040-\u309F\u30A0-\u30FF]', text):
             """ katakana """
-            text = re.sub('[・·=゠＝\u3000 \-、，,]', '・', text)
+            text = re.sub(r'[・·=゠＝\u3000 \-、，,]', '・', text)
         else:
             text = unidecode.unidecode(text)
             text = text.replace('`', "'")
